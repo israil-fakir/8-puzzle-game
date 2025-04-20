862cd0f4c29cd06b571b73acdbd212d90b9a8920
israil-fakir <israilfakirrobin@gmail.com>
2025-04-21 00:38:12 +0600

    pop up okay

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import time
 from behave import *
-from settings import *
+from setting import *
 
 
 class Game:
@@ -11,27 +11,54 @@ class Game:
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption(title)
         self.clock = pygame.time.Clock()
-        self.running = True
-        self.board = Board(gamesize, tilesize, bgcolour)
-        self.sprites = pygame.sprite.Group()
-        self.sprites.add(self.board)
+    #     self.running = True
+    #     self.board = Board(gamesize, tilesize, bgcolour)
+    #     self.sprites = pygame.sprite.Group()
+    #     self.sprites.add(self.board)
+
+    # def run(self):
+    #     while self.running:
+    #         for event in pygame.event.get():
+    #             if event.type == pygame.QUIT:
+    #                 self.running = False
+    #             elif event.type == pygame.KEYDOWN:
+    #                 if event.key == pygame.K_ESCAPE:
+    #                     self.running = False
+    #                 elif event.key == pygame.K_SPACE:
+    #                     self.board.shuffle()
+
+    #         self.screen.fill(bgcolour)
+    #         self.sprites.update()
+    #         self.sprites.draw(self.screen)
+    #         pygame.display.flip()
+    #         self.clock.tick(FPS)
+
+    #     pygame.quit()
+
+    def new(self):
+        pass
 
     def run(self):
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.running = False
-                    elif event.key == pygame.K_SPACE:
-                        self.board.shuffle()
-
-            self.screen.fill(bgcolour)
-            self.sprites.update()
-            self.sprites.draw(self.screen)
-            pygame.display.flip()
+        self.playing = True
+        while self.playing:
             self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
 
-        pygame.quit()
+    def update(self):
+        pass
 
+    def draw(self):
+        pass
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+
+game = Game()
+while True:
+    game.new()
+    game.run()
