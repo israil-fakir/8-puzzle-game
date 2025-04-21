069d68fe1bec3cb42098fed61c14275daaa64329
israil-fakir <israilfakirrobin@gmail.com>
2025-04-21 20:27:16 +0600

    add new colour

--- setting.py
+++ setting.py
@@ -3,11 +3,12 @@ black = (0,0,255)
 darkgray = (50,50,50)
 lightgray = (100,100,100)
 bgcolour = lightgray
+blue = (0,0,255)
 
 # Game settings
 FPS = 60
-WINDOW_WIDTH = 1100
-WINDOW_HEIGHT = 641
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 640
 title = "8 puzzle"
 tilesize = 128
 gamesize = 3
