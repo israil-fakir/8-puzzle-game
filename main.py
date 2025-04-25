956a8ec2bf5cbc2d1ea5f3b28863c8e5e2912862
israil-fakir <israilfakirrobin@gmail.com>
2025-04-26 00:52:06 +0600

    add automated with BFS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+from collections import deque
 from sprite import *
 from setting import *
 
@@ -20,19 +21,14 @@ class Game:
         self.win = 0
         self.high_score = float(self.high_score()[0])
 
-    
     def high_score(self):
         with open("highscore.txt", "r") as file:
             score = file.read().splitlines()
         return score
 
-
     def save_high_score(self):
         with open("highscore.txt", "w") as file:
             file.write(str("%.3f" % self.high_score))
-
-        
-
 
     def create_game(self):
         grid = []
@@ -40,17 +36,17 @@ class Game:
         for x in range(gamesize):
             grid.append([])
             for y in range(gamesize):
-                grid[x].append(number) 
+                grid[x].append(number)
                 number += 1
 
         grid[-1][-1] = 0
         return grid
-    
+
     def random(self):
         possible_move = []
         for row, tiles in enumerate(self.tiles):
             for col, tile in enumerate(tiles):
-                if tile.text == "empty":                                                                     
+                if tile.text == "empty":
                     if tile.right():
                         possible_move.append("right")
                     if tile.left():
@@ -59,57 +55,44 @@ class Game:
                         possible_move.append("up")
                     if tile.dwon():
                         possible_move.append("down")
-                    break    
+                    break
             if len(possible_move) > 0:
                 break
-        
+
         if self.previous_move == "right":
-            possible_move.remove("left") if "left" in possible_move else possible_move
+            if "left" in possible_move:
+                possible_move.remove("left")
         elif self.previous_move == "left":
-            possible_move.remove("right") if "right" in possible_move else possible_move
+            if "right" in possible_move:
+                possible_move.remove("right")
         elif self.previous_move == "up":
-            possible_move.remove("down") if "down" in possible_move else possible_move
+            if "down" in possible_move:
+                possible_move.remove("down")
         elif self.previous_move == "down":
-            possible_move.remove("up") if "up" in possible_move else possible_move
+            if "up" in possible_move:
+                possible_move.remove("up")
 
         choice = random.choice(possible_move)
         self.previous_move = choice
 
-        choice = random.choice(possible_move)
         if choice == "right":
-            self.tiles_grid[row][col], self.tiles_grid[row][col+1] =  self.tiles_grid[row][col+1], self.tiles_grid[row][col]
-        
+            self.tiles_grid[row][col], self.tiles_grid[row][col+1] = self.tiles_grid[row][col+1], self.tiles_grid[row][col]
         elif choice == "left":
-             self.tiles_grid[row][col], self.tiles_grid[row][col-1] =   self.tiles_grid[row][col-1], self.tiles_grid[row][col]
-
+            self.tiles_grid[row][col], self.tiles_grid[row][col-1] = self.tiles_grid[row][col-1], self.tiles_grid[row][col]
         elif choice == "up":
-            self.tiles_grid[row][col], self.tiles_grid[row - 1][col] =   self.tiles_grid[row - 1][col], self.tiles_grid[row][col]
-        
+            self.tiles_grid[row][col], self.tiles_grid[row - 1][col] = self.tiles_grid[row - 1][col], self.tiles_grid[row][col]
         elif choice == "down":
-             self.tiles_grid[row][col], self.tiles_grid[row + 1][col] =   self.tiles_grid[row + 1][col], self.tiles_grid[row][col]           
+            self.tiles_grid[row][col], self.tiles_grid[row + 1][col] = self.tiles_grid[row + 1][col], self.tiles_grid[row][col]
 
-
-
-
-                
-
-    def draw_tiles(self):
+    def draw_tiles(self): 
         self.tiles = []
         for row, x in enumerate(self.tiles_grid):
             self.tiles.append([])
             for col, tile_val in enumerate(x):
-                if tile_val != 0:  
-                    self.tiles[row].append(tile(self, col,row, str(tile_val)))
+                if tile_val != 0:
+                    self.tiles[row].append(tile(self, col, row, str(tile_val)))
                 else:
-                    self.tiles[row].append(tile(self, col,row, "empty"))
-                    
-                
-        # [
-        #     [1,2,3],
-        #     [4,5,6],
-        #     [7,8,0]
-        # ]
-
+                    self.tiles[row].append(tile(self, col, row, "empty"))
 
     def new(self):
         self.all_sprites = pygame.sprite.Group()
@@ -123,9 +106,8 @@ class Game:
         self.button_list = []
         self.button_list.append(button(800, 200, 200, 50, "Random", white, black))
         self.button_list.append(button(800, 280, 200, 50, "Reset", white, black))
+        self.button_list.append(button(750, 360, 300, 50, "Automated (BFS)", white, black))
         self.draw_tiles()
-        # self.test = uielement(1000,450,"test")
-        # self.button = button(400,400, 200, 100, "test", white, black)
 
     def run(self):
         self.playing = True
@@ -136,25 +118,21 @@ class Game:
             self.draw()
 
     def update(self):
-        
         if self.start_game:
             if self.tiles_grid == self.tiles_grid_completed and not self.win:
                 self.start_game = False
                 self.win = True
-                if self.high_score > 0:                  
-                  self.high_score = min(self.high_score, self.elapesd_time)
-
-
+                if self.high_score > 0:
+                    self.high_score = min(self.high_score, self.elapesd_time)
                 else:
-                    self.high_score =self.elapesd_time
+                    self.high_score = self.elapesd_time
                 self.save_high_score()
-
 
             if self.start_timer:
                 self.timer = time.time()
                 self.start_timer = False
             self.elapesd_time = time.time() - self.timer
-               
+
         self.all_sprites.update()
 
         if self.start_random:
@@ -165,16 +143,15 @@ class Game:
                 self.start_random = False
                 self.start_game = True
                 self.start_timer = True
-               
 
+                
 
     def draw_grid(self):
         for row in range(-1, gamesize*tilesize, tilesize):
-            pygame.draw.line(self.screen, blue, (row,0), (row, gamesize*tilesize))
+            pygame.draw.line(self.screen, blue, (row, 0), (row, gamesize*tilesize))
 
         for col in range(-1, gamesize*tilesize, tilesize):
-            pygame.draw.line(self.screen,blue, (0, col), (gamesize*tilesize, col))
-
+            pygame.draw.line(self.screen, blue, (0, col), (gamesize*tilesize, col))
 
     def draw(self):
         self.screen.fill(bgcolour)
@@ -185,11 +162,66 @@ class Game:
 
         # self.test.draw(self.screen)
         # self.button.draw(self.screen)
+
         uielement(600, 0, "High score: %0.3f" % (self.high_score if self.high_score > 0 else 0)).draw(self.screen)
-        uielement(825,70,"%0.3f" % self.elapesd_time).draw(self.screen)
+        uielement(825, 70, "%0.3f" % self.elapesd_time).draw(self.screen)
         if self.win:
             uielement(500, 70, "You win at:").draw(self.screen)
         pygame.display.flip()
+
+    def get_state_from_grid(self):
+        return tuple(tuple(row) for row in self.tiles_grid)
+
+    def animate_solution(self, solution_path):
+        for state in solution_path:
+            self.tiles_grid = [list(row) for row in state]
+            self.draw_tiles()
+            self.draw()
+            pygame.time.delay(300)
+
+    def bfs(self, start_state, goal_state):
+        def get_neighbors(state):
+            neighbors = []
+            size = len(state)
+            for r in range(size):
+                for c in range(size):
+                    if state[r][c] == 0:
+                        empty_pos = (r, c)
+                        break
+            directions = [(-1,0), (1,0), (0,-1), (0,1)]
+            for dr, dc in directions:
+                nr, nc = empty_pos[0] + dr, empty_pos[1] + dc
+                if 0 <= nr < size and 0 <= nc < size:
+                    new_state = [list(row) for row in state]
+                    new_state[empty_pos[0]][empty_pos[1]], new_state[nr][nc] = new_state[nr][nc], new_state[empty_pos[0]][empty_pos[1]]
+                    neighbors.append(tuple(tuple(row) for row in new_state))
+            return neighbors
+
+        queue = deque()
+        visited = set()
+        parent = {}
+
+        queue.append(start_state)
+        visited.add(start_state)
+        parent[start_state] = None
+
+        while queue:
+            current = queue.popleft()
+
+            if current == goal_state:
+                path = []
+                while parent[current] is not None:
+                    path.append(current)
+                    current = parent[current]
+                path.reverse()
+                return path
+
+            for neighbor in get_neighbors(current):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    parent[neighbor] = current
+                    queue.append(neighbor)
+        return []
 
     def events(self):
         for event in pygame.event.get():
@@ -203,18 +235,15 @@ class Game:
                     for col, tile in enumerate(tiles):
                         if tile.click(mouse_x, mouse_y):
                             if tile.right() and self.tiles_grid[row][col+1] == 0:
-                                self.tiles_grid[row][col], self.tiles_grid[row][col+1] =   self.tiles_grid[row][col+1], self.tiles_grid[row][col]
-
+                                self.tiles_grid[row][col], self.tiles_grid[row][col+1] = self.tiles_grid[row][col+1], self.tiles_grid[row][col]
                             if tile.left() and self.tiles_grid[row][col-1] == 0:
-                                self.tiles_grid[row][col], self.tiles_grid[row][col-1] =   self.tiles_grid[row][col-1], self.tiles_grid[row][col]
-
+                                self.tiles_grid[row][col], self.tiles_grid[row][col-1] = self.tiles_grid[row][col-1], self.tiles_grid[row][col]
                             if tile.up() and self.tiles_grid[row - 1][col] == 0:
-                                self.tiles_grid[row][col], self.tiles_grid[row - 1][col] =   self.tiles_grid[row - 1][col], self.tiles_grid[row][col]
-                            
+                                self.tiles_grid[row][col], self.tiles_grid[row - 1][col] = self.tiles_grid[row - 1][col], self.tiles_grid[row][col]
                             if tile.dwon() and self.tiles_grid[row + 1][col] == 0:
-                                self.tiles_grid[row][col], self.tiles_grid[row + 1][col] =   self.tiles_grid[row + 1][col], self.tiles_grid[row][col]
+                                self.tiles_grid[row][col], self.tiles_grid[row + 1][col] = self.tiles_grid[row + 1][col], self.tiles_grid[row][col]
                             self.draw_tiles()
-                                
+
                 for button in self.button_list:
                     if button.click(mouse_x, mouse_y):
                         if button.text == "Random":
@@ -223,8 +252,11 @@ class Game:
 
                         if button.text == "Reset":
                             self.new()
-
-
+                        if button.text == "Automated (BFS)":
+                            start = self.get_state_from_grid()
+                            goal = tuple(tuple(row) for row in self.tiles_grid_completed)
+                            solution_path = self.bfs(start, goal)
+                            self.animate_solution(solution_path)
 
 
 game = Game()
